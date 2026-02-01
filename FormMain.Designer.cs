864b9502ebcc6d676a348aabbcea1ad56b7ad73e
Jacks87 <86174490+Jacks87@users.noreply.github.com>
2026-01-31 20:28:31 -0800

    Initial commit – RandomNotes Notepad
    Initial commit – RandomNotes Notepad

---
+++ FormMain.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace RandomNotes
+{
+    public partial class FormMain : System.Windows.Forms.Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.labelCountdownGuatemala = new System.Windows.Forms.Label();
+            this.pictureBoxGuatemala = new System.Windows.Forms.PictureBox();
+            this.monthCalendarGuatemala = new System.Windows.Forms.MonthCalendar();
+            this.NotePad = new System.Windows.Forms.Label();
+            this.gif = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBoxEditor = new System.Windows.Forms.RichTextBox();
+            this.pictureBoxGif = new System.Windows.Forms.PictureBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGuatemala)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gif)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelCountdownGuatemala
+            // 
+            this.labelCountdownGuatemala.AutoSize = true;
+            this.labelCountdownGuatemala.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.labelCountdownGuatemala.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.labelCountdownGuatemala.Location = new System.Drawing.Point(834, 64);
+            this.labelCountdownGuatemala.Name = "labelCountdownGuatemala";
+            this.labelCountdownGuatemala.Size = new System.Drawing.Size(305, 27);
+            this.labelCountdownGuatemala.TabIndex = 4;
+            this.labelCountdownGuatemala.Text = "Select a Date to See Countdown";
+            // 
+            // pictureBoxGuatemala
+            // 
+            this.pictureBoxGuatemala.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGuatemala.Image")));
+            this.pictureBoxGuatemala.Location = new System.Drawing.Point(72, 364);
+            this.pictureBoxGuatemala.Name = "pictureBoxGuatemala";
+            this.pictureBoxGuatemala.Size = new System.Drawing.Size(322, 184);
+            this.pictureBoxGuatemala.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGuatemala.TabIndex = 3;
+            this.pictureBoxGuatemala.TabStop = false;
+            // 
+            // monthCalendarGuatemala
+            // 
+            this.monthCalendarGuatemala.Location = new System.Drawing.Point(864, 122);
+            this.monthCalendarGuatemala.Name = "monthCalendarGuatemala";
+            this.monthCalendarGuatemala.TabIndex = 2;
+            this.monthCalendarGuatemala.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendarGuatemala_DateChanged);
+            // 
+            // NotePad
+            // 
+            this.NotePad.AutoSize = true;
+            this.NotePad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.NotePad.Location = new System.Drawing.Point(73, 61);
+            this.NotePad.Name = "NotePad";
+            this.NotePad.Size = new System.Drawing.Size(157, 25);
+            this.NotePad.TabIndex = 1;
+            this.NotePad.Text = "Trip Notes Editor";
+            // 
+            // gif
+            // 
+            this.gif.Location = new System.Drawing.Point(0, 0);
+            this.gif.Name = "gif";
+            this.gif.Size = new System.Drawing.Size(100, 50);
+            this.gif.TabIndex = 0;
+            this.gif.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1252, 30);
+            this.menuStrip1.TabIndex = 5;
+            // 
+            // fileToolStripMenuItem1
+            // 
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            this.fileToolStripMenuItem1.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem1.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click_1);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // richTextBoxEditor
+            // 
+            this.richTextBoxEditor.Location = new System.Drawing.Point(72, 101);
+            this.richTextBoxEditor.Name = "richTextBoxEditor";
+            this.richTextBoxEditor.Size = new System.Drawing.Size(635, 228);
+            this.richTextBoxEditor.TabIndex = 0;
+            this.richTextBoxEditor.Text = "";
+            this.richTextBoxEditor.TextChanged += new System.EventHandler(this.richTextBoxEditor_TextChanged);
+            // 
+            // pictureBoxGif
+            // 
+            this.pictureBoxGif.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxGif.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGif.Image")));
+            this.pictureBoxGif.Location = new System.Drawing.Point(864, 364);
+            this.pictureBoxGif.Name = "pictureBoxGif";
+            this.pictureBoxGif.Size = new System.Drawing.Size(262, 184);
+            this.pictureBoxGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGif.TabIndex = 6;
+            this.pictureBoxGif.TabStop = false;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(523, 416);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(184, 49);
+            this.axWindowsMediaPlayer1.TabIndex = 7;
+            // 
+            // FormMain
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1252, 602);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pictureBoxGif);
+            this.Controls.Add(this.richTextBoxEditor);
+            this.Controls.Add(this.NotePad);
+            this.Controls.Add(this.monthCalendarGuatemala);
+            this.Controls.Add(this.pictureBoxGuatemala);
+            this.Controls.Add(this.labelCountdownGuatemala);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "My Trips";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGuatemala)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gif)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // 🔑 REQUIRED FIELD DECLARATIONS (THIS WAS MISSING)
+        private System.Windows.Forms.Label labelCountdownGuatemala;
+        private System.Windows.Forms.PictureBox pictureBoxGuatemala;
+        private System.Windows.Forms.MonthCalendar monthCalendarGuatemala;
+        private System.Windows.Forms.Label NotePad;
+        private System.Windows.Forms.PictureBox gif;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBoxEditor;
+        private System.Windows.Forms.PictureBox pictureBoxGif;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,185 @@
+﻿// ========= DISCLAIMER - last update 1/31/2026 =========
+// This source code was written by me, in my own words, and implemented by me inside my Visual Studio.
+// I did not receive a complete “finished code file” from any other person to submit as-is.
+// I did, however, use ChatGPT as a support tool to help with brainstorming the application idea,
+// discussing GUI component choices, troubleshooting build/runtime issues, and suggesting improvements.
+// All final implementation decisions, edits, and testing were performed by me.
+// ChatGPT helped with debugging guidance only.
+// ================================================
+
+using System;
+using System.Windows.Forms;
+
+namespace RandomNotes
+{
+    public partial class FormMain : Form
+    {
+
+        // Keeps track of the currently opened file path
+        private string currentFilePath = string.Empty;
+
+        // Tracks whether the document has unsaved changes
+        private bool isModified = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // Load background music when the app starts
+            axWindowsMediaPlayer1.URL = "U2 - Beautiful Day.mp3";
+
+            // Loop the song continuously
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+            // Start playing automatically
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+        }
+
+        private void richTextBoxEditor_TextChanged(object sender, EventArgs e)
+        {
+            // Mark the document as modified when the user types
+            isModified = true;
+        }
+
+        private void monthCalendarGuatemala_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTime today = DateTime.Today;
+            DateTime selectedDate = e.Start;
+
+            int daysLeft = (selectedDate - today).Days;
+
+            if (daysLeft > 0)
+            {
+                labelCountdownGuatemala.Text =
+                    daysLeft + " days until your Guatemala trip ✈️";
+            }
+            else if (daysLeft == 0)
+            {
+                labelCountdownGuatemala.Text =
+                    "Your Guatemala trip is today! 🎉";
+            }
+            else
+            {
+                labelCountdownGuatemala.Text =
+                    "This date has already passed.";
+            }
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+
+            // Only show text files by default
+            openDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Load file contents into the editor
+                richTextBoxEditor.Text =
+                    System.IO.File.ReadAllText(openDialog.FileName);
+
+                // Store the file path
+                currentFilePath = openDialog.FileName;
+
+                // Reset modified flag
+                isModified = false;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // If no file has been saved yet, trigger Save As
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                SaveAs();
+            }
+            else
+            {
+                // Overwrite existing file
+                System.IO.File.WriteAllText(
+                    currentFilePath,
+                    richTextBoxEditor.Text);
+
+                isModified = false;
+            }
+        }
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void SaveAs()
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+
+            // Default file type
+            saveDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(
+                    saveDialog.FileName,
+                    richTextBoxEditor.Text);
+
+                currentFilePath = saveDialog.FileName;
+                isModified = false;
+            }
+        }
+
+        // ================= FILE MENU ACTIONS =================
+
+        // NEW — clears the editor
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBoxEditor.Clear();
+        }
+
+        // OPEN — load a .txt or .rtf file
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text Files (*.txt)|*.txt|Rich Text (*.rtf)|*.rtf";
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (openDialog.FileName.EndsWith(".rtf"))
+                    richTextBoxEditor.LoadFile(openDialog.FileName);
+                else
+                    richTextBoxEditor.Text = System.IO.File.ReadAllText(openDialog.FileName);
+            }
+
+        }
+
+        // SAVE — quick save (defaults to Save As)
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            saveAsToolStripMenuItem_Click(sender, e);
+        }
+
+        // SAVE AS — choose file location
+        private void saveAsToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Text Files (*.txt)|*.txt|Rich Text (*.rtf)|*.rtf";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (saveDialog.FileName.EndsWith(".rtf"))
+                    richTextBoxEditor.SaveFile(saveDialog.FileName);
+                else
+                    System.IO.File.WriteAllText(saveDialog.FileName, richTextBoxEditor.Text);
+            }
+
+        }
+
+        // EXIT — close the app
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
